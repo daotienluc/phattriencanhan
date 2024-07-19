45690cc1ad0308b4a11e98227b04a7c89790760f
daotienluc <lucd1177@gmail.com>
2024-07-20 01:58:32 +0700

    xong đường dẫn trang

---
+++ public/js/index.js
@@ -0,0 +1,46 @@
+document.getElementById("btnLogin").onclick = function () {
+  window.location.href = "./login.html";
+};
+
+document.getElementById("btnSignUp").onclick = function () {
+  window.location.href = "./register.html";
+};
+
+// toan10, lí 10, hóa 10
+document.getElementById("toan10").onclick = function () {
+  window.location.href = "./blog_link/toan10.html";
+};
+
+document.getElementById("vatli10").onclick = function () {
+  window.location.href = "./blog_link/li10.html";
+};
+
+document.getElementById("hoahoc10").onclick = function () {
+  window.location.href = "./blog_link/hoa10.html";
+};
+
+// toán 11, lí 11, hóa 11
+document.getElementById("toan11").onclick = function () {
+  window.location.href = "./blog_link/toan11.html";
+};
+
+document.getElementById("vatli11").onclick = function () {
+  window.location.href = "./blog_link/li11.html";
+};
+
+document.getElementById("hoahoc11").onclick = function () {
+  window.location.href = "./blog_link/hoa11.html";
+};
+
+// toán 12, lí 12, hóa 12
+document.getElementById("toan12").onclick = function () {
+  window.location.href = "./blog_link/toan12.html";
+};
+
+document.getElementById("vatli12").onclick = function () {
+  window.location.href = "./blog_link/li12.html";
+};
+
+document.getElementById("hoahoc12").onclick = function () {
+  window.location.href = "./blog_link/hoa12.html";
+};
---
+++ public/js/logged.js
@@ -0,0 +1,38 @@
+// toan10, lí 10, hóa 10
+document.getElementById("toan10").onclick = function () {
+  window.location.href = "./blog_link/toan10-login.html";
+};
+
+document.getElementById("vatli10").onclick = function () {
+  window.location.href = "./blog_link/li10-login.html";
+};
+
+document.getElementById("hoahoc10").onclick = function () {
+  window.location.href = "./blog_link/hoa10-login.html";
+};
+
+// toán 11, lí 11, hóa 11
+document.getElementById("toan11").onclick = function () {
+  window.location.href = "./blog_link/toan11-login.html";
+};
+
+document.getElementById("vatli11").onclick = function () {
+  window.location.href = "./blog_link/li11-login.html";
+};
+
+document.getElementById("hoahoc11").onclick = function () {
+  window.location.href = "./blog_link/hoa11-login.html";
+};
+
+// toán 12, lí 12, hóa 12
+document.getElementById("toan12").onclick = function () {
+  window.location.href = "./blog_link/toan12-login.html";
+};
+
+document.getElementById("vatli12").onclick = function () {
+  window.location.href = "./blog_link/li12-login.html";
+};
+
+document.getElementById("hoahoc12").onclick = function () {
+  window.location.href = "./blog_link/hoa12-login.html";
+};
